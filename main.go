4f089d9a444b9c5421ab16d7554289d596a0579f
Xiu Wang <sylvi.xw@gmail.com>
2020-01-10 12:48:18 -0500

    Testing Git Hook- file change

--- main.go
+++ main.go
@@ -7,4 +7,5 @@ import (
 func main() {
    chocolates := []string{"perk", "munch", "fivestar"}
    fmt.Println(funk.Contains(chocolates, "perk"))
+   ftm.Println("Changing file for hook testing")
 }
